80805be7a98d546191a9d384f25f8fa32f81fac1
C_ffeeStain <61088924+C-ffeeStain@users.noreply.github.com>
2021-03-07 06:52:00 -0600

    only alphanumeric and underscore in username

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+import re
+
 username = input("What's your username? ")
-with open("info.txt","w") as f:
-    f.write(f"name = \"{username}\"\nmoney = \"100\"\ndebug = \"false\"")
+if re.match("^\w+$",username):
+    
+    with open("data.toml","w") as f:
+        f.write(f"""name = \"{username}\"
+money = 100
+debug = false""")
+else:
+    print("Your username can't have anything but letters, numbers, and the underscore.")
